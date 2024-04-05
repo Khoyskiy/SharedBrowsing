79ef4b03a60a239dbcdc250d23a2af748d42144b
khokh <vlodarchuk.kseniia@chnu.edu.ua>
2024-04-05 18:21:55 +0300

    popup work

---
+++ src/main/java/app/sharedbrowsing/SharedBrowsingApplication.java
@@ -0,0 +1,13 @@
+package app.sharedbrowsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedBrowsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedBrowsingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/sharedbrowsing/SharedBrowsingApplicationTests.java
@@ -0,0 +1,13 @@
+package app.sharedbrowsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedBrowsingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
